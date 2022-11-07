02ebf675df2425f494ed4be239a87429a3edd9c8
dibbodas12 <dibbodas228@gmail.com>
2022-11-08 00:47:30 +0600

    slider added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <footer className="footer footer-center p-4 bg-black text-base-content">
         <div>
           <p>Copyright © 2022 - All right reserved by Paradise Photography</p>
         </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 py-8">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -26,7 +26,7 @@ const Header = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-compact dropdown-content font-semibold mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
              <li><Link to='/'>Home</Link></li>
              <li><Link to='/services'>Services</Link></li>
@@ -39,7 +39,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal p-0">
+          <ul className="menu menu-horizontal p-0 font-semibold">
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/services'>Services</Link></li>
           <li><Link to='/blog'>Blog</Link></li>
@@ -47,7 +47,7 @@ const Header = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <button className="btn btn-primary">Get started</button>
+          <button className="btn btn-primary">Sign Up</button>
         </div>
       </div>
     </div>
